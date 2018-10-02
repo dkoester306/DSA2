44c9b37ffa54dbe8edd91492e08d81fe1d16ca76
Danny Koester <dkoester306@gmail.com>
2018-10-01 21:25:24 -0400

    Finished E06.

--- E06 - LERP/AppClass.cpp
+++ E06 - LERP/AppClass.cpp
@@ -2,7 +2,7 @@
 void Application::InitVariables(void)
 {
 	//Change this to your name and email
-	m_sProgrammer = "Alberto Bobadilla - labigm@rit.edu";
+	m_sProgrammer = "Danny Koester - dsk6539@rit.edu";
 
 	//Set the position and target of the camera
 	m_pCameraMngr->SetPositionTargetAndUpward(vector3(5.0f,3.0f,15.0f), ZERO_V3, AXIS_Y);
@@ -47,7 +47,7 @@ void Application::Display(void)
 	m_pModel->PlaySequence();
 
 	//Get a timer
-	static float fTimer = 0;	//store the new timer
+	static double fTimer = 0;	//store the new timer
 	static uint uClock = m_pSystem->GenClock(); //generate a new clock for that timer
 	fTimer += m_pSystem->GetDeltaTime(uClock); //get the delta time for that timer
 
@@ -55,14 +55,32 @@ void Application::Display(void)
 	vector3 v3CurrentPos;
 	
 
-
-
-
 	//your code goes here
-	v3CurrentPos = vector3(0.0f, 0.0f, 0.0f);
-	//-------------------
+	//v3CurrentPos = vector3(0.0f, 0.0f, 0.0f);
+
+	vector3 v3Start;
+	vector3 v3End;
+	static uint route = 0;	// keeps track of what vertex to track.
+	const uint STOP_SIZE = m_stopsList.size();
+
+
+	float fPercentage = static_cast<float>(MapValue(fTimer, 0.0, 2.0, 0.0, 1.0));
+
+	//v3CurrentPos = glm::lerp(v3)
+
+	vector3 startV = m_stopsList[route];
+	vector3 endV = m_stopsList[(route + 1)%STOP_SIZE];
+
+	
+	v3CurrentPos = glm::lerp(startV, endV, fPercentage);
 	
 
+	if (fPercentage >= 1.0f)
+	{
+		route++;
+		fTimer = m_pSystem->GetDeltaTime(uClock);
+		route %= STOP_SIZE;
+	}
 
 	
 	matrix4 m4Model = glm::translate(v3CurrentPos);
@@ -74,7 +92,7 @@ void Application::Display(void)
 	// Draw stops
 	for (uint i = 0; i < m_stopsList.size(); ++i)
 	{
-		m_pMeshMngr->AddSphereToRenderList(glm::translate(m_stopsList[i]) * glm::scale(vector3(0.05f)), C_GREEN, RENDER_WIRE);
+		m_pMeshMngr->AddSphereToRenderList(glm::translate(m_stopsList[i]) * glm::scale(vector3(0.5f)), C_GREEN, RENDER_WIRE);
 	}
 	
 	// draw a skybox
